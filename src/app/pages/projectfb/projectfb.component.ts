0e7d607f3d49b458cb2446ef499dfb777bf2e796
Chu Tuấn Nghĩa <nghia.bst3@gmail.com>
2022-10-12 11:39:30 +0700

    done ui frontend

--- src/app/pages/projectfb/projectfb.component.ts
+++ src/app/pages/projectfb/projectfb.component.ts
@@ -13,15 +13,15 @@ export class ProjectfbComponent implements OnInit {
     ['fb-banner4.jpg','Fulong Tea','Fulong Tea - Fulong Tea','Chủ Đầu Tư: Anh Phong','Quy mô: 24m2','Mức đầu tư: 700 triệu'],
     ['fb-banner5.jpg','Banana King','Banana King - Banana King','Chủ Đầu Tư: Anh Lê Trung Nguyên','Quy mô: 153m2','Mức đầu tư: 1.8 tỷ'],
     ['fb-banner6.jpg','MTP Coffee','MTP Coffee - MTP Coffee','Chủ Đầu Tư: Ms. Thúy','Quy mô: 100 m2','Mức đầu tư: 600 triệu'],
-    ['fb-banner6.jpg','Sành Coffee','Sành Coffee - Sành Coffee','Chủ đầu tư: Anh Lê Văn Long','Quy mô: 100m2','Ngân sách: 900 triệu'],
-    ['fb-banner7.jpg','Gió Coffee','Gió Coffee - Gió Coffee','Chủ đầu tư: Anh Trọng','Quy mô: 250 m2','Mức đầu tư: 900 triệu'],
-    ['fb-banner8.jpg','MOM Tea & Coffee','MOM Tea & Coffee - MOM Tea & Coffee','Chủ đầu tư: Ms. Hiếu','Quy mô: 200m2','Ngân sách: 750 triệu'],
-    ['fb-banner9.jpg','H & T Hong Kong','H & T Hong Kong - H & T Hong Kong','Chủ đầu tư: Ms. Trân','Quy mô: 120m2','Mức đầu tư: 800 triệu'],
-    ['fb-banner10.jpg','Sky Bar','Sky Bar - Sky Bar','Chủ Đầu Tư: Anh Hòa','Quy mô: 570 m2','Mức đầu tư: 3.7 tỷ'],
-    ['fb-banner11.jpg','59 Võ Oanh, Phường 25, Bình Thạnh, Thành phố Hồ Chí Minh','59 Võ Oanh, Phường 25, Bình Thạnh, Thành phố Hồ Chí Minh - Trà Chanh 74','Chủ đầu tư: Ms. Diễm Phạm','Quy mô: 120 m2','Mức đầu tư: 400 triệu'],
-    ['fb-banner12.jpg',"TOYS 'N' DRINKERS","TOYS 'N' DRINKERS - TOYS 'N' DRINKERS",'Chủ đầu tư: Anh Sơn','Quy mô: 80m2','Mức đầu tư: 180 triệu'],
-    ['fb-banner13.jpg','Coffee & Office','Coffee & Office - Oslo Coffee & Office','- Chủ Đầu Tư: Anh Trí','- Quy mô:120m2','- Mức đầu tư: 400 triệu'],
-    ['fb-banner14.jpg','Coffee Shop','Coffee Shop - Hoàng Viên Coffee','- Chủ đầu tư: Chị Hồng','- Quy mô: 150m2','- Mức đầu tư: 500tr']
+    ['fb-banner7.jpg','Sành Coffee','Sành Coffee - Sành Coffee','Chủ đầu tư: Anh Lê Văn Long','Quy mô: 100m2','Ngân sách: 900 triệu'],
+    ['fb-banner8.jpg','Gió Coffee','Gió Coffee - Gió Coffee','Chủ đầu tư: Anh Trọng','Quy mô: 250 m2','Mức đầu tư: 900 triệu'],
+    ['fb-banner9.jpg','MOM Tea & Coffee','MOM Tea & Coffee - MOM Tea & Coffee','Chủ đầu tư: Ms. Hiếu','Quy mô: 200m2','Ngân sách: 750 triệu'],
+    ['fb-banner10.jpg','H & T Hong Kong','H & T Hong Kong - H & T Hong Kong','Chủ đầu tư: Ms. Trân','Quy mô: 120m2','Mức đầu tư: 800 triệu'],
+    ['fb-banner11.jpg','Sky Bar','Sky Bar - Sky Bar','Chủ Đầu Tư: Anh Hòa','Quy mô: 570 m2','Mức đầu tư: 3.7 tỷ'],
+    ['fb-banner12.jpg','59 Võ Oanh, Phường 25, Bình Thạnh, Thành phố Hồ Chí Minh','59 Võ Oanh, Phường 25, Bình Thạnh, Thành phố Hồ Chí Minh - Trà Chanh 74','Chủ đầu tư: Ms. Diễm Phạm','Quy mô: 120 m2','Mức đầu tư: 400 triệu'],
+    ['fb-banner13.jpg',"TOYS 'N' DRINKERS","TOYS 'N' DRINKERS - TOYS 'N' DRINKERS",'Chủ đầu tư: Anh Sơn','Quy mô: 80m2','Mức đầu tư: 180 triệu'],
+    ['fb-banner14.jpg','Coffee & Office','Coffee & Office - Oslo Coffee & Office','- Chủ Đầu Tư: Anh Trí','- Quy mô:120m2','- Mức đầu tư: 400 triệu'],
+    ['fb-banner15.jpg','Coffee Shop','Coffee Shop - Hoàng Viên Coffee','- Chủ đầu tư: Chị Hồng','- Quy mô: 150m2','- Mức đầu tư: 500tr']
   ];
   dataimg = [
     [
@@ -98,7 +98,7 @@ export class ProjectfbComponent implements OnInit {
       ['sc9.jpg'],
       ['sc10.jpg'],
     ],
-    ,[
+    [
       ['gc1.jpg'],
       ['gc2.jpg'],
       ['gc3.jpg'],
@@ -107,8 +107,8 @@ export class ProjectfbComponent implements OnInit {
       ['gc6.jpg'],
       ['gc7.jpg'],
       ['gc8.jpg'],
-    ]
-    ,[
+    ],
+    [
       ['mtc1.jpg'],
       ['mtc2.jpg'],
       ['mtc3.jpg'],
@@ -131,15 +131,15 @@ export class ProjectfbComponent implements OnInit {
       ['mtc20.jpg'],
       ['mtc21.jpg'],
       ['mtc22.jpg'],
-    ]
-    ,[
+    ],
+    [
       ['hk1.jpg'],
       ['hk2.jpg'],
       ['hk3.jpg'],
       ['hk4.jpg'],
       ['hk5.jpg'],
-    ]
-    ,[
+    ],
+    [
       ['sb1.jpg'],
       ['sb2.jpg'],
       ['sb3.jpg'],
@@ -148,8 +148,8 @@ export class ProjectfbComponent implements OnInit {
       ['sb6.jpg'],
       ['sb7.jpg'],
       ['sb8.jpg'],
-    ]
-    ,[
+    ],
+    [
       ['tc1.jpg'],
       ['tc2.jpg'],
       ['tc3.jpg'],
