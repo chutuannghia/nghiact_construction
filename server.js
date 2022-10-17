c9bac0713352b9aeb1a2b9e40a21e4c062b9631a
Chu Tuấn Nghĩa <nghia.bst3@gmail.com>
2022-10-17 15:16:16 +0700

    deploy app with heroku

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/angular-app-heroku'));
 
-app.listen(process.env.PORT || 4200);
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8200);
