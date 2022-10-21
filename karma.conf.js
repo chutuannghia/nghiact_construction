4580ea57315a916ae12c760ed9c7303b89516089
trunghieulam <TkFBUpGRJXT4xK5ZrxUR>
2022-10-21 08:28:37 +0700

    change name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/nghiact_construction'),
+      dir: require('path').join(__dirname, './coverage/newcons'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'nghiact_construction'`, () => {
+  it(`should have as title 'newcons'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('nghiact_construction');
+    expect(app.title).toEqual('newcons');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('nghiact_construction app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('newcons app is running!');
   });
 });
